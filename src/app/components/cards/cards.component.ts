e8227c0e884dfa93550397ed4ea24796917c4c22
debabratasahoo002 <debabrata.sahoo.002@gmail.com>
2019-05-21 17:03:22 +0530

    added removing cards feature.

--- src/app/components/cards/cards.component.ts
+++ src/app/components/cards/cards.component.ts
@@ -1,26 +1,41 @@
-import { AngularFireDatabase } from '@angular/fire/database';
+import { Place } from './../../models/place.model';
+import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
 import { PlaceService } from './../../services/place.service';
 
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component, OnInit, EventEmitter, OnDestroy } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-cards',
   templateUrl: './cards.component.html',
   styleUrls: ['./cards.component.sass']
 })
-export class CardsComponent implements OnInit {
+export class CardsComponent implements OnInit, OnDestroy {
   items: Observable<any[]>;
-  constructor( db: AngularFireDatabase ) { 
-    this.items = db.list('place').valueChanges();
+  itemsRef: AngularFireList<any>;
+  
+  arr:object[];
+  constructor( db: AngularFireDatabase ) {     
+    this.itemsRef = db.list('place');
+    this.items = this.itemsRef.valueChanges();
+    //initailizing snapshot objects for extracting keys
+    this.items = this.itemsRef.snapshotChanges().pipe(
+      map(changes => 
+        changes.map(c => ({ key: c.payload.key, ...c.payload.val() }))
+      )
+    );
   }
-  removePlace(){
-    this.items.subscribe(res => {
-      console.log(res);
-    });
+ 
+  removePlace(key){
+    this.itemsRef.remove(key.key);
+    
   }
 
   ngOnInit() {
+  }
+  ngOnDestroy(){
+    
   }
 
 }
